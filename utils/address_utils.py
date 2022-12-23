22be274b87262389e03287a800a257b1aae87934
minhhn2910 <minhhn2910@gmail.com>
2022-12-23 11:25:40 +0800

    fix proxy of non-standard implementation + add unittest for some erc20

--- utils/address_utils.py
+++ utils/address_utils.py
@@ -5,6 +5,8 @@ from eth_utils import to_hex
 signature_files = {
                     "ERC20": 'signature_data/erc20_signature.json'
                 }
+# default value at uinitialized storage slot
+ZERO_ADDRESS = '0x' + '0'*40
 # Special case when address is a proxy
 proxy_signature = '5c60da1b' # implementation()
 # not the optimal way of doing this because we load json file everytime.
@@ -39,24 +41,23 @@ def classify_address(w3,address):
         # storagePositions = [keccak256("org.zeppelinos.proxy.implementation", bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1))]
         storage_positions = [ "0x7050c9e0f4ca769c69bd3a8ef740bc37934f8e2c036e5a723fd8ee048ed3f8c3",
                               "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"]
-
+        implement_address = ZERO_ADDRESS
         # try every storage position until find the correct one
         for storage_position in storage_positions:
             # find the implement address
             result = w3.eth.getStorageAt(address, storage_position)
             implement_address = '0x' + str(result.hex())[-40:]
             implement_address = w3.toChecksumAddress(implement_address)
-            try:
-                matched_result = classify_address(w3, implement_address)
-            except:
-                continue
-            else:
-                # print("Implement address ", implement_address)
+            if implement_address != ZERO_ADDRESS:
                 break
-        # implementation_addr = w3.eth.call({'value': 0, 'gas': 100000,
-        #                                     'to': address,
-        #                                         'data': '0x'+proxy_signature})
-        # print ("implementation found ", implementation_addr)
+        if implement_address == ZERO_ADDRESS:
+            implement_address = w3.eth.call({'value': 0, 'gas': 100000,
+                                            'to': address,
+                                                'data': '0x'+proxy_signature})
+            if type (implement_address) is not str:
+                implement_address = w3.toChecksumAddress(implement_address.hex()[-40:])
+        print ("implementation found ", implement_address)
+        matched_result = classify_address(w3, implement_address)
     else:
         print (f" found matched results {matched_result} {matched_perc*100}% identical ")
     # print ("debug ", matching_results)
