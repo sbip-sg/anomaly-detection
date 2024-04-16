9226c202e7b3ac5a8abadf6d8fc49fc070cb4d9f
minhhn2910 <minhhn2910@gmail.com>
2024-04-16 15:50:49 +0800

    typo

--- webapp.py
+++ webapp.py
@@ -71,5 +71,5 @@ def process():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
 
