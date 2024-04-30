0ac73193c7630a4224a0b85bca41d7d3920f7072
Ztong55 <ztong55@nus.edu.sg>
2024-04-30 16:02:51 +0800

    Update get_rate.py
    Change timestamp to 8 hours ago if the time is too close.

--- utils/get_rate.py
+++ utils/get_rate.py
@@ -1,10 +1,23 @@
 import ccxt
+from datetime import datetime
 
 coin_dict = {
 	'WETH': 'ETH',
 	'imBTC': 'BTC'
 }
 
+def to_available(timestamp):
+	# Get the current timestamp
+	current_timestamp = datetime.now().timestamp()
+
+	# Calculate the difference between current timestamp and old timestamp
+	time_difference = current_timestamp - timestamp
+
+	# Check if the difference is more than 12 hours
+	if time_difference > 8 * 3600:
+		return timestamp
+	else:
+		return int(current_timestamp - 8 * 3600)
 
 def get_rate(time_stamp, currency):
 	exchange = ccxt.binance()
@@ -13,7 +26,7 @@ def get_rate(time_stamp, currency):
 	if exchange.has['fetchOHLCV']:
 		try:
 			# Example 'BTC/USDT'
-			ohlcv = exchange.fetch_ohlcv(currency + '/USDT', '1d', since=time_stamp * 1000, limit=1)
+			ohlcv = exchange.fetch_ohlcv(currency + '/USDT', '1d', since=to_available(time_stamp) * 1000, limit=1)
 			exchange_rate = ohlcv[0][4]
 		except Exception as e:
 			exchange_rate = 0
