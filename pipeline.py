4ffcc31da940ca74d7d53e1a0810d7865db53ca9
Ztong55 <ztong55@nus.edu.sg>
2024-05-15 19:55:57 +0800

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -50,6 +50,7 @@ def main(tx_hash, chain, overwrite=False):
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("tx_hash", help="Path to the input file")
+	parser.add_argument("chain", help="Transaction chain name")
 	# overwrite existing result
 	parser.add_argument("-o", "--overwrite", action="store_true", help="Overwrite existing result")
 	args = parser.parse_args()
