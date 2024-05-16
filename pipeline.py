bfb13be1c4a7a015256bd1b44e9d567aa424f6b9
Ztong55 <ztong55@nus.edu.sg>
2024-05-16 14:19:55 +0800

    Add token collection

--- pipeline.py
+++ pipeline.py
@@ -3,8 +3,7 @@ import argparse
 from utils.collect_basic_info import collectinfo
 from utils.get_traces import collect_trace
 from utils.decode_trace import decode_trace_json
-from utils.other_balance import collect_token
-from utils.eth_balance import collect_eth
+from utils.token_info import collect_token
 
 rpc_endpoints = {
     "eth": "https://rpc.ankr.com/eth",
@@ -23,7 +22,7 @@ rpc_endpoints = {
 def main(tx_hash, chain, overwrite=False):
 	# Create result directory if it doesn't exist
 	os.makedirs('result', exist_ok=True)
-	folder_prefix = f'result/{tx_hash}_eth'
+	folder_prefix = f'result/{tx_hash}_{chain}'
 	os.makedirs(folder_prefix, exist_ok=True)
 	if (not overwrite) and os.path.exists(f'{folder_prefix}/basic_info.json'):
 		print(f"Result for {tx_hash} already exists. Use -o to overwrite.")
@@ -42,9 +41,7 @@ def main(tx_hash, chain, overwrite=False):
 	decode_trace_json(folder_prefix)
 
 	# Collect token balances
-	#total_dict, flow = collect_token(time_stamp_dict, folder_prefix)
-	# Collect ETH balances
-	#collect_eth(total_dict, time_stamp_dict, flow, folder_prefix)
+	collect_token(time_stamp_dict, chain, rpc_endpoints[chain], folder_prefix)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
---
+++ utils/token_info.py
@@ -0,0 +1,157 @@
+import json
+import pandas as pd
+from os import listdir
+from web3 import Web3
+from utils.get_rate import get_rate
+
+abi_file = open("utils/erc20.abi.json")
+abi = json.load(abi_file)
+
+currencydict = {}
+
+
+def get_currency(hash, w3):
+	if hash not in currencydict.keys():
+		address = Web3.to_checksum_address(hash)
+		contract = w3.eth.contract(
+			address=address,
+			abi=abi,
+		)
+		currency = contract.functions.symbol().call()
+		decimal = contract.functions.decimals().call()
+		currencydict[hash] = (currency, decimal)
+	else:
+		(currency, decimal) = currencydict[hash]
+	return currency, decimal
+
+
+def update_summary(summary, address, currency, amount):
+	if address not in summary:
+		summary[address] = {}
+	if currency not in summary[address]:
+		summary[address][currency] = 0
+
+	summary[address][currency] += amount
+	return summary
+
+
+# Function to handle other transfer transactions
+def othertransfer(summary, currency, from_address, to_address, amount, flow):
+	summary = update_summary(summary, from_address, currency, -amount)
+	summary = update_summary(summary, to_address, currency, amount)
+	flow.loc[len(flow)] = [from_address, to_address, currency, amount]
+
+	return summary
+
+
+# Function to deal with transfer transactions
+def deal_transfer(summary, currency, trace, flow):
+	from_address = trace["from"]
+	to_address = trace["to"]
+	amount = trace["value"]
+
+	summary = update_summary(summary, from_address, currency, -amount)
+	summary = update_summary(summary, to_address, currency, amount)
+	flow.loc[len(flow)] = [from_address, to_address, currency, amount / 1e18]
+
+	return summary
+
+
+# Function to deal with contract creation transactions
+def deal_create(summary, currency, trace, flow):
+	from_address = trace["from"]
+	to_address = trace["to"]
+	amount = trace["value"]
+
+	summary = update_summary(summary, from_address, currency, -amount)
+	summary = update_summary(summary, to_address, currency, amount)
+	flow.loc[len(flow)] = [from_address, to_address, currency, amount / 1e18]
+
+	return summary
+
+
+# Function to deal with selfdestruct transactions
+def deal_selfdestruct(summary, currency, trace, flow):
+	from_address = trace["from"]
+	to_address = trace["refundAddress"]
+	amount = trace["balance"]
+
+	summary = update_summary(summary, from_address, currency, -amount)
+	summary = update_summary(summary, to_address, currency, amount)
+	flow.loc[len(flow)] = [from_address, to_address, currency, amount / 1e18]
+
+	return summary
+def collect_token(timestamp_dict, token, rpc, folder_prefix="result"):
+	total_dict = {}
+	flow = pd.DataFrame(columns=['from', 'to', 'currency', 'value'])
+	jsonlist = listdir(folder_prefix + '/invocation_tree')
+	w3 = Web3(Web3.HTTPProvider(rpc))
+	for i in jsonlist:
+		file = open(folder_prefix + '/event_json/' + i)
+		summary_dict = {}
+		traces = json.load(file)
+		for trace in traces:
+			if trace['type'] == 'event':
+				eventname = trace["function"]
+				input = trace['input']
+				if eventname.lower() == 'transfer':
+					try:
+						currency, decimal = get_currency(trace['address'].lower(), w3)
+					except Exception as e:
+						print(e)
+						currency, decimal = trace['address'].lower(), 0
+					from_address = input[0]
+					if len(input) > 2:
+						to_address = input[1]
+						amount = input[2]
+					elif len(input) == 2:
+						amount = trace['data'][0]
+						to_address = input[1]
+					else:
+						amount = trace['data'][0]
+						to_address = '0x' + '0' * 40
+					summary_dict = othertransfer(summary_dict, currency, from_address, to_address,
+					                             amount / pow(10, decimal), flow)
+				elif eventname.lower() == 'withdrawal' and trace[
+					"address"].lower() == '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2':
+					currency, decimal = get_currency(trace['address'].lower(), w3)
+					from_address = input[0]
+					amount = trace['data'][0]
+					summary_dict = othertransfer(summary_dict, currency, from_address, currency,
+					                             amount / pow(10, decimal), flow)
+				elif eventname.lower() == 'deposit' and trace[
+					"address"].lower() == '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2':
+					currency, decimal = get_currency(trace['address'].lower(), w3)
+					to_address = input[0]
+					amount = trace['data'][0]
+					summary_dict = othertransfer(summary_dict, currency, currency, to_address,
+					                             amount / pow(10, decimal), flow)
+			else:
+				if trace['type'] == 'call' or trace['type'] == 'staticcall' and trace["value"] != 0:
+					summary_dict = deal_transfer(summary_dict, token, trace, flow)
+				elif trace['type'] == 'create' and trace["value"] != 0:
+					summary_dict = deal_create(summary_dict, token, trace, flow)
+				elif trace['type'] == 'suicide' and trace["value"] != 0:
+					summary_dict = deal_selfdestruct(summary_dict, token, trace, flow)
+		if len(traces) != 0:
+			transaction_hash = i.split("_")[1].split(".")[0]
+			time_stamp = timestamp_dict[transaction_hash]
+			for address in summary_dict.keys():
+				address_balance = summary_dict[address]
+				for token in address_balance:
+					value = address_balance[token]
+					rate = get_rate(time_stamp, token)
+					address_balance[token] = [value, value * rate]
+			total_dict[transaction_hash] = summary_dict
+	for key1 in total_dict.keys():
+		for key2 in total_dict[key1].keys():
+			keys_to_delete = [key for key, value in total_dict[key1][key2].items() if value == 0]
+			for key3 in keys_to_delete:
+				del total_dict[key1][key2][key3]
+	for key1 in total_dict.keys():
+		keys_to_delete = [key for key, value in total_dict[key1].items() if value == {}]
+		for key2 in keys_to_delete:
+			del total_dict[key1][key2]
+	with open(folder_prefix + '/balance.json', 'w') as json_file:
+		json.dump(total_dict, json_file, indent=2)
+	flow.to_json(folder_prefix + '/tokenflow.json', orient='records')
