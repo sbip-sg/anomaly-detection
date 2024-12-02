e2b6098a16f207fa8b8fb9e827461b9cad45f681
Ztong55 <149151442+Ztong55@users.noreply.github.com>
2024-12-02 16:34:44 +0800

    Update rule_access_control.py

--- detect_utils/rule_access_control.py
+++ detect_utils/rule_access_control.py
@@ -1,46 +1,52 @@
-from detect_utils.tools import collect_from_file, filter_transaction, check_balance
+from detect_utils.tools import collect_from_file, filter_transaction
 
-MIN_CALL_LENGTH = 6  # the low the more false positives
-assert MIN_CALL_LENGTH > 1
+# Detect whether all calls with state changes have at least a parent with check_opcodes as True
+def check_all_parents(state_changed_list, trace_dict):
+    for key in state_changed_list:
+        parent = key
+        while isinstance(parent, int):
+            if parent not in trace_dict:
+                return False
+            if trace_dict[parent]['callerchecked']:
+                break
+            parent = trace_dict[parent]['parent']
+            if not isinstance(parent, int):
+                return False
+    return True
 
-# Detect whether the trace contains cycling calls
-def has_cycle(xs):
-    n = len(xs)
-    for seq_len in range(2, n // 2 + 1):
-        for i in range(n - seq_len + 1):
-            subsequence = tuple(xs[i:i + seq_len])
-            remaining_calls = xs[i + seq_len:]
-
-            if len(subsequence) >= MIN_CALL_LENGTH and subsequence in zip(
-                    *[remaining_calls[j:] for j in range(seq_len)]):
-                return subsequence
-
-    return None
+# There is any 57(JUMPI) after 33(CALLER)
+def check_opcodes(code_list):
+    if "33" not in code_list:
+        return False
+    caller_index = code_list.index("33")
+    if "57" in code_list[caller_index:]:
+        return True
+    return False
 
 
 # Detector containing two checks to be considered as possible hack:
-# 1. cyclic calls in transactions: each sequence of calls with minimum length MIN_CALL_LENGTH
-# 2. if the sender's balance changes by more than 10k USD
-def detect_cyclic_transaction(tx_hash, chain):
+def detect_access_control(tx_hash, chain):
     basic_info = collect_from_file(tx_hash, chain, '/basic_info.json')
     if not filter_transaction(basic_info):
         return False
-
-    sender = basic_info.get('from')
-
-    trace = collect_from_file(tx_hash, chain, '/invocation_tree/decode_trace_' + tx_hash + '.json')
-    functions = []
-
-    for call in trace:
-        if call['type'] == 'event' or 'create' in call['type'] or call['type'] == 'selfdestruct':
-            pass  # ignore event in this detector
-        elif 'call' in call['type']:
-            functions.append((call['from'], call['to'], call['function']))
-        else:
-            print(f"Unknown trace type: {call['type']}")
-
-    possible_hack = False
-    if has_cycle(functions) is not None:
-        possible_hack = check_balance(tx_hash, chain, sender)
-
+    trace_dict = {}
+    state_changed_list = []
+    
+    trace = collect_from_file(tx_hash, chain, '/trace_json/trace_' + tx_hash + '.json')
+    for t in trace:
+        if 'call' in t['kind']:
+            state_changed = bool(t['statechanges'])
+            if state_changed:
+                state_changed_list.append(t['call_idx'])
+            parent = t['parent']
+            children = t['children']
+            opcodes = t['opcodes']
+            trace_dict[t['call_idx']] = {
+                'statechanged': state_changed,
+                'parent': parent,
+                'children': children,
+                'opcodes': opcodes,
+                'callerchecked': check_opcodes(opcodes)
+            }
+    possible_hack = not check_all_parents(state_changed_list, trace_dict)
     return possible_hack
