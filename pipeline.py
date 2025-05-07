a7b6602efff7ad97725c4e3d5a148a3dc286e271
Ztong55 <149151442+Ztong55@users.noreply.github.com>
2025-05-07 14:22:06 +0800

    Add outputs

--- pipeline.py
+++ pipeline.py
@@ -4,11 +4,11 @@ from utils.collect_basic_info import collect_info
 from utils.get_traces import collect_trace
 from utils.decode_trace import decode_trace_json
 from utils.token_info import collect_token
+from utils.foundry_to_graph import foundry_to_graph
 from utils.get_rpc import EndpointPool
 from utils.generate_output import generate_output
 from detect_utils.detect_all import rule_based_detection
 from detect_utils.chatgpt_detect import chatgpt_detect
-from detect_utils.deepseekv3_detect import deepseekv3_detect
 import json
 
 
@@ -34,6 +34,7 @@ def main(tx_hash, chain, overwrite=False, use_llm=False):
 
     # Collect traces (raw invocation tree)
     collect_trace(tx_hash, edpool, folder_prefix)
+    foundry_to_graph(folder_prefix)
 
     # Decode trace JSON and extract information from invocation tree
     decode_trace_json(folder_prefix)
@@ -41,16 +42,11 @@ def main(tx_hash, chain, overwrite=False, use_llm=False):
     # According to the decoded invocation tree, get token flow and balance changes.
     main_token = collect_token(tx_hash, chain, basic_info['from'], basic_info['to'], basic_info['blocknumber'], edpool, folder_prefix)
 
-    detection_result, reason = rule_based_detection(tx_hash, folder_prefix)
-
-    basic_info['detection_result'] = detection_result
-    basic_info['reason'] = reason
-
     # Save basic information
     with open(folder_prefix + '/basic_info.json', 'w') as json_file:
         json.dump(basic_info, json_file, indent=2)
 
-    generate_output(tx_hash, chain, folder_prefix, main_token)
+    generate_output(tx_hash, chain, basic_info, folder_prefix, main_token)
 
     # if use_chatgpt and detection_result:
     if use_llm:
