c5d2395944d20348f890c52b93a6d7e036d3b1b4
Ztong55 <149151442+Ztong55@users.noreply.github.com>
2025-06-09 20:40:27 +0800

    Update collect_block.py

--- utils/collect_block.py
+++ utils/collect_block.py
@@ -1,41 +1,13 @@
-import os
-import json
-import subprocess
-from requests.exceptions import HTTPError
-
-cast_bin = os.environ.get("CAST_BIN", "cast")
-
-# collect block using foundry cast
-def cast_block_run(block_number, edpool):
-    rpc_url = edpool.endpoint_by_chain()
-    while True:
-        try:
-            # define the command
-            command = [
-                "cast", "block", str(block_number),
-                "--rpc-url", rpc_url, "--json"
-            ]
-
-            # run the command and capture the output
-            block_result = subprocess.run(command, capture_output=True, text=True, check=True)
-            break
-        except HTTPError as e:
-            # Handle HTTP errors
-            rpc_url = edpool.mark_endpoint_broken(rpc_url)
-            print(f'Error processing request: {e}\n retry ... ')
-
-        except Exception as e:
-            # Handle other unexpected exceptions
-            raise RuntimeError(f"An unexpected error occurred: {e}")
-
-    # load block related data
-    text_output = block_result.stdout.strip()
-    block_data = json.loads(text_output)
-
-    return block_data
+from web3 import Web3
 
 # collect all transactions' envInfo and stats from one block
 def collect_block_all(block_number, endpoint):
     # get all transactions from the block
-    block_tx_list = cast_block_run(block_number, endpoint).get("transactions", [])
+    w3 = Web3(Web3.HTTPProvider(endpoint))
+    block_tx_list = []
+    try:
+        block = w3.eth.get_block(int(block_number), full_transactions=False)
+        block_tx_list = block['transactions']
+    except Exception as e:
+        print(f'unable to find transactions in {block_number}:', e)
     return block_tx_list
